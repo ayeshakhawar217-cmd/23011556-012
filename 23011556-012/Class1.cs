87c53ec759204781cb11cd066742700006a62951
ayesha khawar <ayeshakhawar217@gmail.com>
2025-10-04 16:41:46 +0500

    Exercise 01

--- 23011556-012/Class1.cs
+++ 23011556-012/Class1.cs
@@ -10,12 +10,17 @@ namespace _23011556_012
     {
         public void Run()
         {
+            // Enter the number to be checked
             Console.WriteLine("Enter a number: ");
             int num = Convert.ToInt32(Console.ReadLine());
+
+            // If num is multiple of 2 then display Even
             if(num%2 == 0)
             {
                 Console.WriteLine("Number is Even");
             }
+
+            // If not then display Odd
             else
             {
                 Console.WriteLine("Number is Odd");
---
+++ 23011556-012/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23011556_012
+{
+    internal class Exercise2
+    {
+        public void Run()
+        {
+            Console.WriteLine("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
